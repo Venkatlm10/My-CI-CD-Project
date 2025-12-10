0e9037a4e1057d604494248bebb7d9a6408cffa9
Venkatesh Ram V.K <141840473+Venkatlm10@users.noreply.github.com>
2025-12-10 17:41:29 +0530

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Starting deployment to AWS EC2..."
 
-ssh -i ~/.ssh/id_rsa ubuntu@<13.200.169.238> << 'EOF'
+ssh -i ~/.ssh/id_rsa ubuntu@13.200.169.238 << 'EOF'
   cd /var/www/app
   git pull origin main
   ./restart.sh
