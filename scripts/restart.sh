32816d9cbf656abf10ca0d98631ec237958a1038
Venkatesh Ram V.K <141840473+Venkatlm10@users.noreply.github.com>
2025-12-13 01:06:58 +0530

    Update restart.sh

--- scripts/restart.sh
+++ scripts/restart.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 ./scripts/cleanup.sh
 ./scripts/deploy.sh
+docker stop $(docker ps -q) || true
+docker rm $(docker ps -aq) || true
+docker build -t nginx-app:$BUILD_NUMBER .
+docker run -d -p 80:80 nginx-app:$BUILD_NUMBER
